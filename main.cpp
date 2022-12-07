3949d6e6011987b0ed7d38a8aa7be20259e0acd5
Stevie <mail@steviegoodman.com>
2022-12-07 14:34:09 +0000

    Added command tokenizer function

--- main.cpp
+++ main.cpp
@@ -16,78 +16,100 @@ GOOD LUCK!
 
 ------------------------------------------------------ */
 
-#include "Shape.h"
-#include "Circle.h"
-#include "Rectangle.h"
-#include "Square.h"
-#include "Movable.h"
+using namespace std;
+
 
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
+//#include "Shape.h"
+//#include "Circle.h"
+//#include "Rectangle.h"
+//#include "Square.h"
+//#include "Movable.h"
 
-using namespace std;
+
+/// Takes a string and parses it into a command and command arguments. 
+/// Returns a tuple, with the first element being the command, and the second being the vector of arguments.
+tuple<string, vector<string>> tokenize(const string &userCommand) {
+    tuple<string, vector<string>> result;
+    stringstream stream(userCommand);
+    string current_word;
+    string command;
+    vector<string> arguments;
+    while (stream >> current_word) {
+        arguments.push_back(current_word);
+    }
+    command = arguments.front();
+    arguments.erase(arguments.begin()); // Removes the command from the arguments vector.
+    return make_tuple(command, arguments);
+}
+
+/// Prompts user for a command and returns the user's case insensitive input.
+/// Tags: lambda functions, functional programming
+string &getCommand(string &userCommand) {
+    cout << "Please type in your command: ";
+    getline(cin, userCommand);
+    transform(userCommand.begin(), userCommand.end(), userCommand.begin(), 
+              [](char character){ return tolower(character); });
+    return userCommand;
+}
 
 int main()
 {
-    string userCommand;
-    vector <Shape*> shapes;     // this one will hold your shapes
-    vector <string> parameters; // this one will hold parameters for the commands
-
-
-    while (userCommand.compare("exit") != 0)
+    cout << "=== SHAPE MANIPULATOR ===" << endl;
+    
+    while (true) // Repeats the prompt until player exits the program using the "exit" command.
     {
-        cout << "Enter the command: ";
-
-        getline(cin, userCommand);
-
-        char * cstr = new char[userCommand.length() + 1];
-
-        strcpy_s(cstr, userCommand.length()+1, userCommand.c_str());
-
-        // implement a string tokenizer to populate the parameters vector 
-        // check function strtok_s
-
-        // as a result of the process, parameters[0] should hold your command, followed by your parameters 
-        string command = parameters[0];
-
-
-        // in the following code, consider checking for the arguments.
-        // in case of too few arguments, you may remind the user the correct format
-
-        if (command.compare("addR") == 0) {
-            // get parameters in the correct order
-            // The following four lines have a type mismatch error
-            // note that the the parameters vector contains ascii values
-            // HINT: stoi function converts from string to int
-
-            x = parameters[1].c_str(); // fix me! also note that x is not previously defined :(
-            // int y = ...
-            // int h = ...
-            // int w = ...
-
-
-            Rectangle* r = new Rectangle(x, y, h, w);
-            shapes.push_back(r);
-            cout << r->toString(); /* instead of this, you may implement operator overloadig and 
-									use cout << r which will give you additional points */
+        // Get user input
+        string userCommand = getCommand(userCommand);
+        tuple<string, vector<string>> token_tuple = tokenize(userCommand);
+        string command           = get<0>(token_tuple);
+        vector<string> arguments = get<1>(token_tuple);
+        
+        // Parse commands
+        if (command == "exit") {
+            // Exits the program.
+            cout << "Exiting shape manipulator!" << endl;
+            break;
         }
-        else if (command.compare("addS") == 0) {
-            // get parameters
-            // ...
-            Square* s = new Square(x, y, e);
-            shapes.push_back(s);
-            cout << s->toString();
+        else if (command == "addr") {
+            cout << "Adding rectangle!" << endl;
+//            // get parameters in the correct order
+//            // The following four lines have a type mismatch error
+//            // note that the parameters vector contains ascii values
+//            // HINT: stoi function converts from string to int
+//
+//            x = parameters[1].c_str(); // fix me! also note that x is not previously defined :(
+//            // int y = ...
+//            // int h = ...
+//            // int w = ...
+//
+//
+//            Rectangle* r = new Rectangle(x, y, h, w);
+//            shapes.push_back(r);
+//            cout << r->toString(); /* instead of this, you may implement operator overloadig and 
+//									use cout << r which will give you additional points */
         }
-
-        if (command.compare("addC") == 0) {
-            // get parameters
-            // ...
-            Circle* c = new Circle(x, y, r);
-            shapes.push_back(c);
-            cout << c->toString();
-
+        else if (command == "adds") {
+            cout << "Adding square!" << endl;
+//            // get parameters
+//            // ...
+//            Square* s = new Square(x, y, e);
+//            shapes.push_back(s);
+//            cout << s->toString();
         }
-        else if (command.compare("scale") == 0) {
+        else if (command == "addc") {
+            cout << "Adding circle!" << endl;
+//            // get parameters
+//            // ...
+//            Circle* c = new Circle(x, y, r);
+//            shapes.push_back(c);
+//            cout << c->toString();
+        }
+        else if (command == "scale") {
+            cout << "Scaling shape!" << endl;
             // scale object at index... the scaling needs to be isotropic in case of circle and square 
             // you may want to check if the index exists or not!
 
@@ -96,32 +118,36 @@ int main()
             // You may need to use type casting wisely to use polymorphic functionality!
 
         }
-        else if (command.compare("move") == 0) {
-            // move object at index 
-            int shapeNo; // read from parameters
-            // you may want to check if the index exists or not!
-
-            // Study the following code. A Shape object is not Movable, but all derived classes are...
-            // you can't automatically type cast from a Shape to a Movable, but you can force a downcasting
-            Movable *m = dynamic_cast<Movable*>(shapes[shapeNo - 1]);
-            m->move(x, y);
-            // scale should work similarly...
-
-            // note that here you should see the corresponding toString output for the derived classes...
-            // if toString is not a virtual function, you may see the base class functionality :(
-            cout << shapes[shapeNo - 1]->toString();
+        else if (command == "move") {
+            cout << "Moving shape!" << endl;
+//            // move object at index 
+//            int shapeNo; // read from parameters
+//            // you may want to check if the index exists or not!
+//
+//            // Study the following code. A Shape object is not Movable, but all derived classes are...
+//            // you can't automatically type cast from a Shape to a Movable, but you can force a downcasting
+//            Movable *m = dynamic_cast<Movable*>(shapes[shapeNo - 1]);
+//            m->move(x, y);
+//            // scale should work similarly...
+//
+//            // note that here you should see the corresponding toString output for the derived classes...
+//            // if toString is not a virtual function, you may see the base class functionality :(
+//            cout << shapes[shapeNo - 1]->toString();
         }
-        else if (command.compare("display") == 0) {
+        else if (command == "display") {
+            cout << "Displaying list of shapes!" << endl;
             // this is not given in our example, but why don't you implement a display function which shows all objects stored in shapes?
+        }
+        else {
+            cout << "Command not recognised. Please enter a valid command." << endl;
         }
 
         // do any necessary postprocessing at the end of each loop...
         // yes, there is some necessary postprocessing...
-        cout << endl << endl;
+        cout << endl;
     }
-
-    cout << "Press any key to continue...";
-    std::getchar();
-
+    
     return 0;
 }
+
+
