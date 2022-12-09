8ffb822e6962d9554cc5ef43b712244a6f7d21b8
Stevie <mail@steviegoodman.com>
2022-12-09 12:26:12 +0000

    Added header for `Shape` class

--- src/shapes/shape/shape.h
+++ src/shapes/shape/shape.h
@@ -1,9 +1,34 @@
 #ifndef ASSESSMENT_2_SHAPE_H
 #define ASSESSMENT_2_SHAPE_H
 
+using namespace std;
 
+#include <vector>
+#include "../point/point.h"
+
+/// Abstract class that all other shape classes derive functionality and fields from.
 class Shape {
-
+public:
+    /// Calculates the area of the shape and mutates the private member field _area accordingly.
+    virtual void calculateArea() = 0;
+    /// Calculates the perimeter of the shape and mutates the private member field _perimeter accordingly.
+    virtual void calculatePerimeter() = 0;
+    /// Calculates the position of each vertex and mutates the private member field _points accordingly.
+    virtual void calculatePoints() = 0;
+    /// Returns a string containing characteristics of the shape.
+    virtual string toString() = 0;
+    
+private:
+    /// The total enclosed area of this shape.
+    float _area;
+    /// The total surface area of this shape.
+    float _perimeter;
+    /// Used for determining if the shape is circular.
+    bool _isCircular;
+    /// Top-left-most point of the shape.
+    Point _leftTop;
+    /// Vector containing all the vertices of the shape.
+    vector<Point *> _points;
 };
 
 
