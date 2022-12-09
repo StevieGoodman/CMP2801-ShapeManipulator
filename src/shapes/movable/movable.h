96463cd39a994bb1ec155201f8897b25e3f9e606
Stevie <mail@steviegoodman.com>
2022-12-09 12:33:55 +0000

    Implemented header file for `Movable` class

--- src/shapes/movable/movable.h
+++ src/shapes/movable/movable.h
@@ -1,9 +1,15 @@
 #ifndef ASSESSMENT_2_MOVABLE_H
 #define ASSESSMENT_2_MOVABLE_H
 
+#include "../point/point.h"
 
+/// Abstract class that all shape classes derive from the implement movement functionality.
 class Movable {
-
+public:
+    /// Mutates the shape's private _leftTop and _points fields.
+    virtual void move(Point newLeftTop) = 0;
+    /// Mutates the shape's size by multiplying its current size with a given multiplier.
+    virtual void scale(float multiplier) = 0;
 };
 
 
