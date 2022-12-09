156ce9fc445efa7c6d0ef52a9b80fcef8bab9aa2
Stevie <mail@steviegoodman.com>
2022-12-09 11:47:36 +0000

    Added header and implementation for `Point` class

--- src/shapes/point/point.cpp
+++ src/shapes/point/point.cpp
@@ -1,2 +1,11 @@
 #include "point.h"
 
+Point::Point(float x, float y) : _x(x), _y(y) {}
+
+float Point::getX() const {
+    return _x;
+}
+
+float Point::getY() const {
+    return _y;
+}
--- src/shapes/point/point.h
+++ src/shapes/point/point.h
@@ -1,9 +1,21 @@
 #ifndef ASSESSMENT_2_POINT_H
 #define ASSESSMENT_2_POINT_H
 
-
+/// Represents a point in space on a 2D plane.
 class Point {
-    
+public:
+    /// Creates a new Point object with a given set of x and y coordinates.
+    Point(float x, float y);
+    /// Getter method for private member field _x.
+    float getX() const;
+    /// Getter method for private member field _y.
+    float getY() const;
+
+private:
+    /// Horizontal component of position in 2D space.
+    const float _x;
+    /// Vertical component of position in 2D space.
+    const float _y;
 };
 
 
