f7143a9123b100a4661eeed1cddd9ea997b5e371
Stevie <mail@steviegoodman.com>
2022-12-10 12:36:43 +0000

    Added destructor method to `Plane` class

--- src/shapes/plane/plane.cpp
+++ src/shapes/plane/plane.cpp
@@ -1,1 +1,17 @@
 #include "plane.h"
+
+void Plane::addShape(Shape *shape) {
+
+}
+
+void Plane::removeShape(int index) {
+
+}
+
+Plane::~Plane() {
+    std::for_each( // I could have use a for loop but wanted to demonstrate my understanding of iterators and lambdas.
+            this->_shapes.begin(), 
+            this->_shapes.end(), 
+            [](Shape* shape) { delete shape; }
+            );
+}
--- src/shapes/plane/plane.h
+++ src/shapes/plane/plane.h
@@ -1,9 +1,21 @@
 #ifndef ASSESSMENT_2_PLANE_H
 #define ASSESSMENT_2_PLANE_H
 
+#include <vector>
+#include "../shape/shape.h"
 
+/// Represents a container for 2D objects.
 class Plane {
+public:
+    /// Inserts a shape to the plane.
+    void addShape(Shape* shape);
+    /// Removes a shape from the plane.
+    void removeShape(int index);
+    /// Performs memory management on shape objects in private member field _shapes.
+    virtual ~Plane();
 
+private:
+    vector<Shape*> _shapes;
 };
 
 
