6fa97f997381d3cad4255f8777a8065854d4202a
Stevie <mail@steviegoodman.com>
2022-12-11 20:27:00 +0000

    Added `Rectangle` class

--- src/shapes/rectangle/rectangle.cpp
+++ src/shapes/rectangle/rectangle.cpp
@@ -1,1 +1,65 @@
 #include "rectangle.h"
+
+Rectangle::Rectangle(Point *leftTop, float height, float width)
+        : Shape(leftTop), _height(height), _width(width) {
+    this->name = __func__;
+    this->_isCircular = false;
+    calculateArea();
+    calculatePerimeter();
+    calculatePoints();
+}
+
+void Rectangle::calculateArea() {
+    this->_area = _width * _height;
+}
+
+void Rectangle::calculatePerimeter() {
+    this->_perimeter = 2 * (_width + _height);
+}
+
+void Rectangle::calculatePoints() {
+    Point* leftTop     = this->_leftTop;
+    Point* rightTop    = *this->_leftTop + Point(this->_width, 0);
+    Point* leftBottom  = *this->_leftTop + Point(0, this->_height);
+    Point* rightBottom = *this->_leftTop + Point(this->_width, this->_height);
+    this->_points = vector<Point*>{
+            leftTop,
+            rightTop,
+            rightBottom,
+            leftBottom
+    };
+}
+
+string Rectangle::toString() {
+    string width     = to_string(this->_width);
+    string height    = to_string(this->_height);
+    string position  = this->_leftTop->toString();
+    string points    =
+            this->_points.at(0)->toString() + ", " +
+            this->_points.at(1)->toString() + ", " +
+            this->_points.at(2)->toString() + ", " +
+            this->_points.at(3)->toString();
+    string area      = to_string(this->_area);
+    string perimeter = to_string(this->_perimeter);
+    string circular  = (to_string(this->_isCircular) == "1") ? "Yes" : "No";
+    return "Position: " + position
+           + "\nWidth: " + width
+           + "\nHeight: " + height
+           + "\nArea: " + area
+           + "\nPerimeter: " + perimeter
+           + "\nCircular: " + circular
+           + "\nPoints: " + points;
+}
+
+void Rectangle::move(Point *newLeftTop) {
+    this->_leftTop = newLeftTop;
+    this->calculatePoints();
+}
+
+void Rectangle::scale(float xMultiplier, float yMultiplier) {
+    this->_width *= xMultiplier;
+    this->_height *= yMultiplier;
+    calculatePoints();
+    calculateArea();
+    calculatePerimeter();
+}
--- src/shapes/rectangle/rectangle.h
+++ src/shapes/rectangle/rectangle.h
@@ -1,9 +1,24 @@
 #ifndef ASSESSMENT_2_RECTANGLE_H
 #define ASSESSMENT_2_RECTANGLE_H
 
+#include "../shape/shape.h"
+#include "../movable/movable.h"
 
-class Rectangle {
+class Rectangle : public Shape, public Movable {
+public:
+    Rectangle(Point *leftTop, float height, float width);
+    void calculateArea() override;
+    void calculatePerimeter() override;
+    void calculatePoints() override;
+    string toString() override;
+    void move(Point *newLeftTop) override;
+    void scale(float xMultiplier, float yMultiplier) override;
 
+private:
+    /// Vertical size of the rectangle.
+    float _height;
+    /// Horizontal size of the rectangle.
+    float _width;
 };
 
 
